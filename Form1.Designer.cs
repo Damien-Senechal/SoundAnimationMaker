a2f1df389883662774936c41ea5aa5f9a9cde7d2
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2020-11-24 17:08:14 +0100

    merge du main

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,26 +32,170 @@ namespace SoundAnimationMaker
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer_Son = new System.Windows.Forms.Timer(this.components);
             this.timer_Image = new System.Windows.Forms.Timer(this.components);
+            this.combox = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonParametre = new System.Windows.Forms.PictureBox();
+            this.buttonParcourir = new System.Windows.Forms.PictureBox();
+            this.buttonClose = new System.Windows.Forms.PictureBox();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.nom = new System.Windows.Forms.PictureBox();
+            this.buttonLancer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonParametre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonParcourir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonLancer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // combox
+            // 
+            this.combox.FormattingEnabled = true;
+            this.combox.Location = new System.Drawing.Point(472, 395);
+            this.combox.Name = "combox";
+            this.combox.Size = new System.Drawing.Size(196, 21);
+            this.combox.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(472, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 289);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // buttonParametre
+            // 
+            this.buttonParametre.BackColor = System.Drawing.Color.Transparent;
+            this.buttonParametre.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonParametre.Image = ((System.Drawing.Image)(resources.GetObject("buttonParametre.Image")));
+            this.buttonParametre.Location = new System.Drawing.Point(674, 395);
+            this.buttonParametre.Name = "buttonParametre";
+            this.buttonParametre.Size = new System.Drawing.Size(125, 25);
+            this.buttonParametre.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.buttonParametre.TabIndex = 3;
+            this.buttonParametre.TabStop = false;
+            this.buttonParametre.Click += new System.EventHandler(this.buttonParametre_Click);
+            this.buttonParametre.MouseLeave += new System.EventHandler(this.buttonParametre_MouseLeave);
+            this.buttonParametre.MouseHover += new System.EventHandler(this.buttonParametre_MouseHover);
+            // 
+            // buttonParcourir
+            // 
+            this.buttonParcourir.BackColor = System.Drawing.Color.Transparent;
+            this.buttonParcourir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonParcourir.Image = ((System.Drawing.Image)(resources.GetObject("buttonParcourir.Image")));
+            this.buttonParcourir.Location = new System.Drawing.Point(803, 329);
+            this.buttonParcourir.Name = "buttonParcourir";
+            this.buttonParcourir.Size = new System.Drawing.Size(125, 25);
+            this.buttonParcourir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.buttonParcourir.TabIndex = 4;
+            this.buttonParcourir.TabStop = false;
+            this.buttonParcourir.Click += new System.EventHandler(this.buttonParcourir_Click);
+            this.buttonParcourir.MouseLeave += new System.EventHandler(this.buttonParcourir_MouseLeave);
+            this.buttonParcourir.MouseHover += new System.EventHandler(this.buttonParcourir_MouseHover);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.Transparent;
+            this.buttonClose.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonClose.BackgroundImage")));
+            this.buttonClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonClose.Image = ((System.Drawing.Image)(resources.GetObject("buttonClose.Image")));
+            this.buttonClose.Location = new System.Drawing.Point(803, 460);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(125, 25);
+            this.buttonClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.buttonClose.TabIndex = 5;
+            this.buttonClose.TabStop = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            this.buttonClose.MouseLeave += new System.EventHandler(this.buttonClose_MouseLeave);
+            this.buttonClose.MouseHover += new System.EventHandler(this.buttonClose_MouseHover);
+            // 
+            // logo
+            // 
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(12, 33);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(420, 149);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo.TabIndex = 6;
+            this.logo.TabStop = false;
+            // 
+            // nom
+            // 
+            this.nom.BackColor = System.Drawing.Color.Transparent;
+            this.nom.Image = ((System.Drawing.Image)(resources.GetObject("nom.Image")));
+            this.nom.Location = new System.Drawing.Point(73, 188);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(292, 222);
+            this.nom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.nom.TabIndex = 7;
+            this.nom.TabStop = false;
+            // 
+            // buttonLancer
+            // 
+            this.buttonLancer.BackColor = System.Drawing.Color.Transparent;
+            this.buttonLancer.Image = ((System.Drawing.Image)(resources.GetObject("buttonLancer.Image")));
+            this.buttonLancer.Location = new System.Drawing.Point(472, 435);
+            this.buttonLancer.Name = "buttonLancer";
+            this.buttonLancer.Size = new System.Drawing.Size(250, 50);
+            this.buttonLancer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.buttonLancer.TabIndex = 8;
+            this.buttonLancer.TabStop = false;
+            this.buttonLancer.Click += new System.EventHandler(this.buttonLancer_Click);
+            this.buttonLancer.MouseLeave += new System.EventHandler(this.buttonLancer_MouseLeave);
+            this.buttonLancer.MouseHover += new System.EventHandler(this.buttonLancer_MouseHover);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 404);
+            this.ClientSize = new System.Drawing.Size(940, 497);
+            this.Controls.Add(this.buttonLancer);
+            this.Controls.Add(this.nom);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonParcourir);
+            this.Controls.Add(this.buttonParametre);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.combox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SoundAnimationMaker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonParametre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonParcourir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonLancer)).EndInit();
             this.ResumeLayout(false);
-            this.FormBorderStyle = FormBorderStyle.None;
 
         }
 
         #endregion
         private System.Windows.Forms.Timer timer_Son;
         private System.Windows.Forms.Timer timer_Image;
+        private ComboBox combox;
+        private PictureBox pictureBox1;
+        private PictureBox buttonParametre;
+        private PictureBox buttonParcourir;
+        private PictureBox buttonClose;
+        private PictureBox logo;
+        private PictureBox nom;
+        private PictureBox buttonLancer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -14,78 +7,29 @@ namespace SoundAnimationMaker
 {
     public partial class Form1 : Form
     {
+        Image bgImg = Image.FromFile("Ressources/fondForm.png");
+
+        Image parametrePushed = Image.FromFile("Ressources/parametrePushed.png");
+        Image parametre = Image.FromFile("Ressources/parametre.png");
+
+        Image closePushed = Image.FromFile("Ressources/closePushed.png");
+        Image close = Image.FromFile("Ressources/close.png");
+
+        Image parcourirPushed = Image.FromFile("Ressources/parcourirPushed.png");
+        Image parcourir = Image.FromFile("Ressources/parcourir.png");
+
+        Image lancerPushed = Image.FromFile("Ressources/lancerPushed.png");
+        Image lancer = Image.FromFile("Ressources/lancer.png");
+
         public Form1()
         {
             InitializeComponent();
-            //Son.ScanSoundCards(comboBox1);
-            // 
-            // ecranAccueil
-            // 
-            PictureBox ecranAccueil = new PictureBox
-            {
-                Name = "ecranAccueil",
-                Size = new Size(ClientSize.Width, ClientSize.Height),
-                Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - ClientSize.Width, Screen.PrimaryScreen.Bounds.Height / 2 - ClientSize.Height),
-                BackgroundImage = Image.FromFile("Ressources/fond.png"),
-                SizeMode = PictureBoxSizeMode.AutoSize,
-
-            };
-            this.Controls.Add(ecranAccueil);
-            ecranAccueil.BringToFront();
-            // 
-            // fenetrePrincipale
-            // 
-            PictureBox fenetrePrincipale = new PictureBox
-            {
-                Name = "fenetrePrincipale",
-                Size = new Size(100, 50),
-                Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2),
-                //Image = Image.FromFile("claude.jpg"),
-
-            };
-            this.Controls.Add(fenetrePrincipale);
-            // 
-            // fenetrePrincipale
-            //
-            PictureBox fenetreGif = new PictureBox
-            {
-                Name = "fenetreGif",
-                Size = new Size(100, 50),
-                Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2),
-                //Image = Image.FromFile("claude.jpg"),
-
-            };
-            this.Controls.Add(fenetreGif);
-            // 
-            // fenetreTertiaire
-            //
-            PictureBox fenetreTertiaire = new PictureBox
-            {
-                Name = "fenetreTertiaire",
-                Size = new Size(100, 50),
-                Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2),
-                //Image = Image.FromFile("claude.jpg"),
-
-            };
-            this.Controls.Add(fenetreTertiaire);
-            // 
-            // button1
-            // 
-            Button boutonLancer = new Button
-            {
-                Location = new Point(159, 205),
-                Name = "button1",
-                Size = new Size(122, 43),
-                UseVisualStyleBackColor = true,
-                Image = Image.FromFile("Ressources/boutonLancer.png"),
-                FlatStyle = FlatStyle.Popup,
-            };
-            this.Controls.Add(boutonLancer);
-
-            //this.ClientSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-            //fenetrePrincipale.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Son.ScanSoundCards(combox);
+            Size = new Size(Screen.PrimaryScreen.Bounds.Width/2, Screen.PrimaryScreen.Bounds.Height/2);
+            BackgroundImage = bgImg;
         }
 
+        
 
 
         private void timer_Son_Tick(object sender, EventArgs e)
@@ -96,28 +40,97 @@ namespace SoundAnimationMaker
 
         private void timer_Image_Tick(object sender, EventArgs e)
         {
+            
             //GestionImage lancement = new GestionImage(pictureBox);
             //Controleur.GererImage(lancement);
         }
 
-        /*
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            //Son.AudioMonitorInitialize(comboBox1.SelectedIndex);
-            Thread.Sleep(50);
-            timer_Image.Start();
+
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void buttonParametre_MouseHover(object sender, EventArgs e)
+        {
+            buttonParametre.Image = parametrePushed;
+        }
+
+        private void buttonParametre_MouseLeave(object sender, EventArgs e)
+        {
+            buttonParametre.Image = parametre;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonClose_MouseHover(object sender, EventArgs e)
+        {
+            buttonClose.Image = closePushed;
+        }
+
+        private void buttonClose_MouseLeave(object sender, EventArgs e)
+        {
+            buttonClose.Image = close;
+        }
+
+        private void buttonParcourir_MouseLeave(object sender, EventArgs e)
+        {
+            buttonParcourir.Image = parcourir;
+        }
+
+        private void buttonParcourir_MouseHover(object sender, EventArgs e)
+        {
+            buttonParcourir.Image = parcourirPushed;
+        }
+
+        private void buttonParametre_Click(object sender, EventArgs e)
+        {
+            var controlPanelPath = System.IO.Path.Combine(Environment.SystemDirectory, "control.exe");
+            System.Diagnostics.Process.Start(controlPanelPath, "mmsys.cpl");
+        }
+
+        private void buttonParcourir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "png|*.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                pictureBox1.Image = image;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
-            if (Son.wvin != null)
-            {
-                Son.wvin.StopRecording();
-                Son.wvin = null;
-            }
-            timer_Image.Stop();
-        }*/
+        }
 
+        private void buttonLancer_MouseHover(object sender, EventArgs e)
+        {
+            buttonLancer.Image = lancerPushed;
+        }
+
+        private void buttonLancer_MouseLeave(object sender, EventArgs e)
+        {
+            buttonLancer.Image = lancer;
+        }
+
+        private void buttonLancer_Click(object sender, EventArgs e)
+        {
+            buttonParametre.Visible = false;
+            buttonParametre.Visible = false;
+            buttonParcourir.Visible = false;
+            combox.Visible = false;
+            pictureBox1.Visible = false;
+            buttonLancer.Visible = false;
+            logo.Visible = false;
+            nom.Visible = false;
+            this.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            buttonClose.Location = new Point(Screen.PrimaryScreen.Bounds.Width-130, Screen.PrimaryScreen.Bounds.Height-30);
+        }
     }
 }
