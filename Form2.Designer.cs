0e14a99d50e91b30c25fcc073982d19a8a4dc78d
Morgan-ABATECOLA <morgan.abatecola@etu.umontpellier.fr>
2020-11-25 17:03:30 +0100

    ajout du form de début

---
+++ Form2.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace SoundAnimationMaker
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.buttonClose = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.Transparent;
+            this.buttonClose.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonClose.BackgroundImage")));
+            this.buttonClose.Location = new System.Drawing.Point(663, 413);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(125, 25);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.TabStop = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click_1);
+            this.buttonClose.MouseLeave += new System.EventHandler(this.buttonClose_MouseLeave);
+            this.buttonClose.MouseHover += new System.EventHandler(this.buttonClose_MouseHover);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClose);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.buttonClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox buttonClose;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SoundAnimationMaker
+{
+    public partial class Form2 : Form
+    {
+        private Image closePushed = Image.FromFile("Ressources/closePushed.png");
+        private Image close = Image.FromFile("Ressources/close.png");
+        private Form1 frmParent;
+        public Form2(Form1 frm)
+        {
+            
+            InitializeComponent();
+            frmParent = frm;
+            Image imageDeFondForm2 = frm.pictureBox1.Image;
+            PictureBox fondForm2 = new PictureBox();
+            fondForm2.Image = imageDeFondForm2;
+            fondForm2.SizeMode = PictureBoxSizeMode.StretchImage;
+            fondForm2.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            this.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            buttonClose.Location = new Point(Screen.PrimaryScreen.Bounds.Width - 135, Screen.PrimaryScreen.Bounds.Height - 35);
+            fondForm2.BringToFront();
+            this.Controls.Add(fondForm2);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClose_Click_1(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();            
+            this.Hide();
+            this.Close();
+        }
+
+        private void buttonClose_MouseHover(object sender, EventArgs e)
+        {
+            buttonClose.Image = closePushed;
+        }
+
+        private void buttonClose_MouseLeave(object sender, EventArgs e)
+        {
+            buttonClose.Image = close;
+        }
+    }
+}
